f68f54e904df17da8f4059fe81242551a2aaa4d6
angela <angela.valdebenito.v@gmail.com>
2021-10-14 00:09:28 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,28 @@
 <!-- cuando ingresa el usuario -->
 <template>
-  <v-container class="fluid mt-5" >
-    <v-card
-      class="mx-auto"
-      max-width="344"
-      v-for="curso in cursos"
-      :key="curso.id"
-    >
-      <v-img height="200px"></v-img>
+  <v-app>
+    <v-container fluid class="mt-5">
+      <v-row>
+        <v-col v-for="curso in cursos" :key="curso.id" cols="12" sm="3" md="4">
+          <v-card class="mx-auto" max-width="344">
+            <v-img :src="curso.imagen" height="200px"></v-img>
 
-      <v-card-title></v-card-title>
+            <v-card-title class="text-center">{{ curso.nombre }}</v-card-title>
 
-      <v-card-subtitle></v-card-subtitle>
-      <v-card-text></v-card-text>
-    </v-card>
-  </v-container>
-</template>
+            <v-card-subtitle>{{ curso.descripcion }}</v-card-subtitle>
+            <v-card-text>Costo: {{ curso.costo }} </v-card-text>
+            <v-card-text>Duracion: {{ curso.duracion }}</v-card-text>
+            <v-card-text>Cupos: {{ curso.cupos }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 
-<!-- <v-container>
+  <!-- <v-container>
       <pre>{{ cursos }}</pre>
     </v-container> -->
+</template>
 
 <script>
 import Firebase from "firebase";
